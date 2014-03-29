d5bf899898621648f8c3051933d0f0d885401552
David Fowler <davidfowl@gmail.com>
2014-03-29 03:38:40 -0700

    Skip entropy

--- src/SanityCheck/Program.cs
+++ src/SanityCheck/Program.cs
@@ -37,7 +37,8 @@ namespace SanityCheck
                 "MusicStore",
                 "Coherence",
                 "latest-dev",
-                "xunit"
+                "xunit",
+                "Entropy"
             };
 
             foreach (var projectFolder in di.EnumerateDirectories())
