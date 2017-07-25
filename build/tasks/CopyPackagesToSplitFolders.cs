f7fcc555ffa897ef1c115f3d2c9fd4220752e6be
Nate McMaster <nate.mcmaster@microsoft.com>
2017-07-24 18:14:46 -0700

    Update the bootstrapper to install the compiled version of KoreBuild
     - Replace SplitPackages as a package reference with a RepoTask instead

---
+++ build/tasks/CopyPackagesToSplitFolders.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Framework;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using RepoTasks.Utilities;
+
+namespace RepoTasks
+{
+    public class CopyPackagesToSplitFolders : Microsoft.Build.Utilities.Task
+    {
+        /// <summary>
+        /// The item group containing the nuget packages to split in different folders.
+        /// </summary>
+        [Required]
+        public ITaskItem[] Packages { get; set; }
+
+        /// <summary>
+        /// Path to the CSV file containing the names and subfolders where each package must go.
+        /// </summary>
+        [Required]
+        public string CsvFile { get; set; }
+
+        /// <summary>
+        /// The folder where packages should be copied. Subfolders will be created based on package category.
+        /// </summary>
+        [Required]
+        public string DestinationFolder { get; set; }
+
+        public override bool Execute()
+        {
+            if (Packages?.Length == 0)
+            {
+                Log.LogError("No packages were found.");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(CsvFile) || !File.Exists(CsvFile))
+            {
+                Log.LogError($"Package manifest (csv file) could not be loaded from '{CsvFile}'");
+                return false;
+            }
+
+            var expectedPackages = PackageCollection.DeserializeFromCsv(CsvFile);
+
+            Directory.CreateDirectory(DestinationFolder);
+
+            foreach (var package in Packages)
+            {
+                PackageIdentity identity;
+                using (var reader = new PackageArchiveReader(package.ItemSpec))
+                {
+                    identity = reader.GetIdentity();
+                }
+
+                if (!expectedPackages.TryGetCategory(identity.Id, out var category))
+                {
+                    Log.LogError($"{CsvFile} does not contain an entry for a package with id: {identity.Id}");
+                    return false;
+                }
+
+                string destDir;
+                switch (category)
+                {
+                    case PackageCategory.Unknown:
+                        throw new InvalidOperationException($"Package {identity} does not have a recognized package category.");
+                    case PackageCategory.Shipping:
+                        destDir = Path.Combine(DestinationFolder, "ship");
+                        break;
+                    case PackageCategory.NoShip:
+                        destDir = Path.Combine(DestinationFolder, "noship");
+                        break;
+                    case PackageCategory.ShipOob:
+                        destDir = Path.Combine(DestinationFolder, "shipoob");
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+
+                Directory.CreateDirectory(destDir);
+
+                var destFile = Path.Combine(destDir, Path.GetFileName(package.ItemSpec));
+
+                Log.LogMessage($"Copying {package.ItemSpec} to {destFile}");
+
+                File.Copy(package.ItemSpec, destFile);
+                expectedPackages.Remove(identity.Id);
+            }
+
+            if (expectedPackages.Count != 0)
+            {
+                var error = new StringBuilder();
+                foreach (var key in expectedPackages.Keys)
+                {
+                    error.Append(" - ").AppendLine(key);
+                }
+
+                Log.LogError($"Expected the following packages based on the contents of {CsvFile}, but they were not found:" + error.ToString());
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ build/tasks/Utilities/PackageCollection.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RepoTasks.Utilities
+{
+    public class PackageCollection
+    {
+        private readonly IDictionary<string, PackageCategory> _packages = new Dictionary<string, PackageCategory>(StringComparer.OrdinalIgnoreCase);
+
+        private PackageCollection()
+        {
+        }
+
+        public bool TryGetCategory(string packageId, out PackageCategory category) => _packages.TryGetValue(packageId, out category);
+
+        public void Remove(string packageId) => _packages.Remove(packageId);
+
+        public int Count => _packages.Count;
+
+        public IEnumerable<string> Keys => _packages.Keys;
+
+        public static PackageCollection DeserializeFromCsv(string filepath)
+        {
+            using (var stream = File.OpenRead(filepath))
+            using (var reader = new StreamReader(stream))
+            {
+                return DeserializeFromCsv(reader);
+            }
+        }
+
+        public static PackageCollection DeserializeFromCsv(TextReader reader)
+        {
+            var lineNo = 0;
+            var list = new PackageCollection();
+
+            string[] Split(string l) => l.Split(',').Select(s => s.Trim()).ToArray();
+
+            // Package,Category,...
+            // Microsoft.AspNetCore.Razor.Tools,ship,...
+            // Microsoft.VisualStudio.Web.CodeGeneration.Tools,ship,...
+            // Microsoft.Extensions.SecretManager.Tools,ship,...
+
+            var line = reader.ReadLine();
+            lineNo++;
+            var columns = Split(line);
+
+            EnsureHeaderColumns(columns);
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineNo++;
+
+                var values = Split(line);
+                if (values.Length < 2)
+                {
+                    throw new FormatException($"Error on line {lineNo}. Too few columns. Expected at least two.");
+                }
+
+                PackageCategory category;
+                switch (values[1].ToLowerInvariant())
+                {
+                    case "ship":
+                        category = PackageCategory.Shipping;
+                        break;
+                    case "noship":
+                        category = PackageCategory.NoShip;
+                        break;
+                    case "shipoob":
+                        category = PackageCategory.ShipOob;
+                        break;
+                    default:
+                        category = PackageCategory.Unknown;
+                        break;
+                }
+
+                if (list._packages.ContainsKey(values[0]))
+                {
+                    throw new InvalidDataException($"Duplicate package id detected: {values[0]} on line {lineNo}");
+                }
+
+                list._packages.Add(values[0], category);
+            }
+
+            return list;
+        }
+
+        private static void EnsureHeaderColumns(string[] columns)
+        {
+            if (columns.Length < 2)
+            {
+                throw new FormatException("Unrecognized number of columns in csv file");
+            }
+
+            if (!columns[0].Equals("Package", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new FormatException("Unrecognized column: " + columns[0]);
+            }
+
+            if (!columns[1].Equals("Category", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new FormatException("Unrecognized column: " + columns[0]);
+            }
+        }
+    }
+}
